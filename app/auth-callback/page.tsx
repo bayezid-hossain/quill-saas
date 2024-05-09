702291f614ad722f90086d50095e90c71d170e73
forsaken <forsaken@shadow.lan>
2024-05-09 02:51:52 -0400

    added: kinde api for authentication added: prisma

--- app/auth-callback/page.tsx
+++ app/auth-callback/page.tsx
@@ -1,17 +1,31 @@
+'use client';
 import { useRouter, useSearchParams } from 'next/navigation';
 import React from 'react';
 import { trpc } from '../_trpc/client';
+import { Loader2 } from 'lucide-react';
 
 const page = async () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const origin = searchParams.get('origin');
 
-  const { data, isLoading, isError, isSuccess } = trpc.authCallback.useQuery();
+  const { data, isLoading, isError, isSuccess, error } =
+    trpc.authCallback.useQuery(undefined, { retry: true, retryDelay: 500 });
   if (isSuccess) {
     router.push(origin ? `/${origin}` : '/dashboard');
   }
-  return <div>page</div>;
+  if (isError) {
+    if (error.data?.code === 'UNAUTHORIZED') router.push('/sign-in');
+  }
+  return (
+    <div className="w-full mt-24 flex justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+        <h3 className="font-semibold text-xl">Setting up your account....</h3>
+        <p>You will be redirected automatically</p>
+      </div>
+    </div>
+  );
 };
 
 export default page;
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+import Dashboard from '@/components/Dashboard';
+import { db } from '@/db';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { redirect } from 'next/navigation';
 import React from 'react';
@@ -6,7 +8,13 @@ const page = async () => {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
   if (!user || !user.id) redirect('/auth-callback?origin=dashboard');
-  return <div>{user.email}</div>;
+  const dbUser = await db.user.findFirst({
+    where: {
+      id: user.id,
+    },
+  });
+  if (!dbUser) redirect('/auth-callback?origin=dashboard');
+  return <Dashboard />;
 };
 
 export default page;
